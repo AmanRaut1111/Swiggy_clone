b32b003c2640c6f4d81a5245c5b2705bc5687ccc
Aman Raut <amanraut1111@gmail.com>
2024-02-23 23:44:57 +0530

    Add Routers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+
+import Contact from "./components/Contact";
 
 const Applayout = () => {
     return (
         <div className="App">
             <Header />
-            <Body />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <Applayout />,
+        children: [
+
+            {
+                path: '/',
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ],
+        errorElement: <Error />
+
+    },
+
+
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter} />);
