ad3d7d851ace989e61380428029ebdfb4caa815c
Aman Raut <amanraut1111@gmail.com>
2024-02-23 12:15:38 +0530

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <div className="footer">
+                <h3>&copy; Copyright 2024 NamasteFood.Com</h3>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer
