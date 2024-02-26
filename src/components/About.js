9f9056a8e29c8471cf343a74fc9a62df0465e265
Aman Raut <amanraut1111@gmail.com>
2024-02-26 13:11:17 +0530

    change  in about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,59 @@
-const About = () => {
-    return (
+import User from './User'
 
-        <div>
-            <h1> This is About Page By Namste React</h1>
-        </div>
-    )
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+    constructor(props) {
+        super(props)
 
 
+        console.log(" parent constructor is called");
+    }
+
+
+    componentDidMount() {
+        console.log("parent componentDidMount called");
+    }
+    render() {
+
+        console.log("parent render is called");
+        return (
+            <div>
+                <h1> This is About Page By Namste React</h1>
+                {/* <User
+                    name={"Aman Raut  ( functional)"}
+                    address={"gokak"}
+                    contact={"8856051111"}
+                /> */}
+                <UserClass
+                    name={"Aman Raut (class)"}
+                    address={"pune"}
+                    contact={"8180090786"}
+                />
+            </div>
+        );
+    }
 }
 
 
 
-export default About
+// const About = () => {
+//   return (
+//     <div>
+//       <h1> This is About Page By Namste React</h1>
+//       <User
+//         name={"Aman Raut  ( functional)"}
+//         address={"gokak"}
+//         contact={"8856051111"}
+//       />
+//       <UserClass
+//         name={"Aman Raut (class)"}
+//         address={"pune"}
+//         contact={"8180090786"}
+//       />
+//     </div>
+//   );
+// };
+
+export default About;
