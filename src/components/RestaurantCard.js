5032986ca4c8a94ff87acf40c6af8cb7b357f1a3
Aman Raut <amanraut1111@gmail.com>
2024-03-01 12:02:12 +0530

    Add promotted label to restoCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,6 +3,8 @@ import { CDN_URL } from "../utils/constants.js";
 const RestaurantCard = (props) => {
     const { resData } = props;
 
+
+
     return (
         <div className="res-card">
             <img
@@ -13,11 +15,30 @@ const RestaurantCard = (props) => {
             <h3>{resData.info.name}</h3>
             <h4>{resData.info.cuisines.join(" ,")}</h4>
             <h4> {resData.info.deliveryTime} </h4>
-            <h4>{resData.info.avgRating}</h4>
+            <h4>
+                {resData.info.avgRating} <span className="fa fa-star checked"></span>{" "}
+            </h4>
 
             <h4> {resData.info.costForTwo} </h4>
         </div>
     );
 };
+
+
+
+//higher order component
+
+
+export const withPromottedLabel = (RestaurantCard) => {
+    return (props) => {
+        return (
+            <div>
+                <label className="label">Promotted</label>
+                <RestaurantCard {...props}></RestaurantCard>
+            </div>
+        )
+    }
+}
+
 
 export default RestaurantCard;
