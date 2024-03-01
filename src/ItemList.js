81a4fdc44f8b683ed8648e4cbb2602ffdb2254e3
Aman Raut <amanraut1111@gmail.com>
2024-03-01 12:00:18 +0530

    add component

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,27 +1,32 @@
+import { CDN_URL } from "../src/utils/constants";
+
 const ItemList = ({ items }) => {
-
-    return <div>
+    return (
         <div>
-            {items.map((item) => {
-                return (
-                    <div className="acc-box" key={item.card.info.id}>
-                        <div>
-
-                            <span>{item.card.info.name} </span>
-                            <span>{item.card.info.price} </span>
-
-
+            <div>
+                {items.map((item) => {
+                    return (
+                        <div className="acc-box" key={item.card.info.id}>
+                            <div className="acc-by">
+                                <img
+                                    className="acc-img"
+                                    src={CDN_URL + item?.card?.info?.imageId}
+                                />
+                                <div>
+                                    <button className="ac-btn"> Add +</button>
+                                </div>
+                            </div>
+                            <div>
+                                <span>{item.card.info.name} </span>
+                                <span> ₹ {item.card.info.price / 100} </span>
+                            </div>
+                            <p>{item.card.info.description}</p>
                         </div>
-
-                        <p>{item.card.info.description}</p>
-                    </div>
-                )
-
-
-            })}
+                    );
+                })}
+            </div>
         </div>
-    </div>
+    );
+};
 
-}
-
-export default ItemList
+export default ItemList;
