6dc58d96afb9429f1dc81d82d58ea31477fc50aa
Aman Raut <amanraut1111@gmail.com>
2024-03-01 12:03:11 +0530

    add online /offline feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants.js";
 
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus.js";
 
 const Header = () => {
     const [btnReact, setbtnReact] = useState("Login");
 
-
+    const onlineStatus = useOnlineStatus()
     useEffect(() => {
 
     }, [])
@@ -19,9 +20,12 @@ const Header = () => {
 
             <div className="nav-items">
                 <ul>
+
+                    <li>  {onlineStatus ? <span> Online🟢 </span> : <span> Offline 🔴</span>}</li>
                     <li><Link to="/" className="Link">Home</Link></li>
                     <li> <Link className="Link" to='/about'> About us</Link></li>
                     <li><Link className="Link" to="/contact" >contact us</Link></li>
+
                     <li>cart</li>
                     <button
                         className="login-btn"
