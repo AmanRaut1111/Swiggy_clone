7f6df4268e4d9d800fb05a255410ff2855b34522
Aman Raut <amanraut1111@gmail.com>
2024-03-01 11:51:26 +0530

    cjange in line 6

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,29 @@
+import ItemList from "../ItemList";
+
+import { useState } from "react";
+
+const RestaurantCategory = ({ data }) => {
+
+    const [showitems, setShowitems] = useState(false)
+    const handleClick = () => {
+        setShowitems(!showitems)
+    };
+
+
+    return <div className=" acc">
+
+        <div className="accodian-container">
+            <div className="acc-conatiner" onClick={handleClick}>
+                <h3 className="as">{data.title} ({data.itemCards.length})</h3>
+                <span>⬇️</span>
+
+            </div>
+            {showitems && <ItemList items={data.itemCards} />}
+
+        </div>
+
+
+    </div>
+}
+
+export default RestaurantCategory
